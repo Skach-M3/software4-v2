8fc4a77b1780ecc6b065b2e3000c8bfe73609b38
Lukstr Wu <tifaswift29@gmail.com>
2023-08-09 21:49:08 +0800

    软件介绍完成

--- src/components/tab/SoftwareIntro.vue
+++ src/components/tab/SoftwareIntro.vue
@@ -1,16 +1,44 @@
 <template>
   <div>
     <div class="container">
-      <div style="font-size: 50px">多病种疾病风险预测工具软件</div>
-      <div class="size-icon">
+      <div style="font-size: 50px">多病种疾病危险因素关联关系挖掘工具软件</div>
+      <div class="size-icon1">
         <div v-for="(item, index) in quickEntry" :key="index" class="icon">
-          <div class="singleBox" @click="quickLink(index)">
-            <img
-              :src="item.img"
-              class="imgStyle"
-              style="border-radius: 15px"
-            />
+          <div class="singleBox" @click="quickLink(item.router)">
+            <img :src="item.img" class="imgStyle" style="border-radius: 15px" />
             <h3 style="font-size: 30px">{{ item.title }}</h3>
+          </div>
+        </div>
+        <div class="size-icon2">
+          <div class="icon">
+            <div class="singleBox" @click="quickLink('/DisFactor')">
+              <img
+                src="@/assets/danLiYuCe.png"
+                class="imgStyle"
+                style="border-radius: 15px"
+              />
+              <h3 style="font-size: 30px">疾病危险因素挖掘</h3>
+            </div>
+          </div>
+          <div class="icon">
+            <div class="singleBox" @click="quickLink('/F_Factor')">
+              <img
+                src="@/assets/batch.png"
+                class="imgStyle"
+                style="border-radius: 15px"
+              />
+              <h3 style="font-size: 30px">危险因素相关因素挖掘</h3>
+            </div>
+          </div>
+          <div class="icon">
+            <div class="singleBox" @click="quickLink('/FactorDis')">
+              <img
+                src="@/assets/-research.png"
+                class="imgStyle"
+                style="border-radius: 15px"
+              />
+              <h3 style="font-size: 30px">危险因素相关疾病挖掘</h3>
+            </div>
           </div>
         </div>
       </div>
@@ -20,7 +48,7 @@
 <script>
 export default {
   setup() {},
-  data(){
+  data() {
     return {
       quickEntry: [
         {
@@ -31,27 +59,17 @@ export default {
         {
           title: "任务管理",
           img: require("@/assets/model_intro.png"),
-          router: "",
+          router: "/TaskManage",
         },
-        {
-          title: "疾病危险因素挖掘",
-          img: require("@/assets/danLiYuCe.png"),
-          router: "/Predict",
-        },
-        {
-          title: "危险因素相关因素挖掘",
-          img: require("@/assets/batch.png"),
-          router: "/Predict",
-        }
       ],
-    }
+    };
   },
 
-  methods:{
-    quickLink(index) {
-      this.$router.push(this.quickEntry[index].router);
+  methods: {
+    quickLink(router) {
+      this.$router.push(router);
     },
-  }
+  },
 };
 </script>
 <style scoped>
@@ -62,15 +80,28 @@ export default {
   height: 80vh;
   flex-flow: column;
 }
-.size-icon {
+.size-icon1 {
   font-size: 80px;
   display: grid;
 
   grid-template-columns: repeat(2, 300px);
-  grid-template-rows: repeat(2, 300px);
+  /* grid-template-rows: repeat(2, 300px); */
   gap: 5px;
   justify-items: center;
   align-items: center;
+  margin-top: 30px;
+}
+.size-icon2 {
+  font-size: 80px;
+  display: grid;
+
+  grid-template-columns: repeat(3, 400px);
+  /* grid-template-rows: repeat(2, 300px); */
+  gap: 5px;
+  justify-items: center;
+  align-items: center;
+  margin-top: 50px;
+  margin-left: 17vw;
 }
 .icon {
   display: grid;
@@ -83,5 +114,9 @@ export default {
 }
 .icon h3 {
   margin-top: 10px;
+}
+.imgStyle {
+  height: 150px;
+  width: 150px;
 }
 </style>
