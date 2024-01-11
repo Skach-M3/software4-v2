9290553c6587ef9b278b57c1c3a1cc4ee2fa6814
rzzz-ssq <1814616195@qq.com>
2024-01-11 10:10:24 +0800

    选择数据界面demo

---
+++ src/components/tab/dataSelectTree.js
@@ -0,0 +1,69 @@
+export const dataSelectTree = [
+    {
+    id: '1',
+    label: '糖尿病',
+    children: [{
+      id: '1-1',
+      label: '一型糖尿病',
+      children: [{
+        id: '1-1-1',
+        label: '数据集1'
+      },{
+        id: '1-1-2',
+        label: '数据集2'
+      },{
+        id: '1-1-3',
+        label: '数据集3'
+      },{
+        id: '1-1-4',
+        label: '数据集4'
+      }]
+    },{
+        id: '1-2',
+        label: '二型糖尿病',
+        children: [{
+            id: '1-2-1',
+            label: '数据集1'
+          },{
+            id: '1-2-2',
+            label: '数据集2'
+          },{
+            id: '1-2-3',
+            label: '数据集3'
+          }]
+    }]
+    },
+    {
+        id: '2',
+        label: '肺病',
+        children: [{
+          id: '2-1',
+          label: 'xxx肺病',
+          children: [{
+            id: '2-1-1',
+            label: '数据集1'
+          },{
+            id: '2-1-2',
+            label: '数据集2'
+          },{
+            id: '2-1-3',
+            label: '数据集3'
+          },{
+            id: '2-1-4',
+            label: '数据集4'
+          }]
+        },{
+            id: '2-2',
+            label: 'xx肺病',
+            children: [{
+                id: '2-2-1',
+                label: '数据集1'
+              },{
+                id: '2-2-2',
+                label: '数据集2'
+              },{
+                id: '2-2-3',
+                label: '数据集3'
+              }]
+        }]
+        }]
