7324c588a605755c9a25d53dbfb25e45e16ce811
Lukstr Wu <tifaswift29@gmail.com>
2024-04-12 20:23:15 +0800

    在模糊查询那次提交引入了虚拟列表组件导致无法运行，删除引入

--- src/main.js
+++ src/main.js
@@ -6,18 +6,18 @@ import ElementUI from "element-ui";
 import echarts from "echarts";
 import "element-ui/lib/theme-chalk/index.css";
 import axios from "axios";
-import 'xe-utils'
-import VXETable from 'vxe-table'
-import 'vxe-table/lib/index.css'
+// import 'xe-utils'
+// import VXETable from 'vxe-table'
+// import 'vxe-table/lib/index.css'
  
-import UmyUi from 'umy-ui'
-import 'umy-ui/lib/theme-chalk/index.css';// 引入样式
+// import UmyUi from 'umy-ui'
+// import 'umy-ui/lib/theme-chalk/index.css';// 引入样式
  
-Vue.use(UmyUi);
+// Vue.use(UmyUi);
 Vue.prototype.$echarts = echarts;
 Vue.prototype.$axios = axios;
 Vue.config.productionTip = false;
-Vue.use(VXETable)
+// Vue.use(VXETable)
 Vue.use(ElementUI);
 new Vue({
   router,
